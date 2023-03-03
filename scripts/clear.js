fc884d5960f41a2f711a650b9b8e5372b5287963
Louis Beaumont <louis.beaumont@gmail.com>
2023-03-03 11:02:26 +0000

    fix: chunk indexing

--- scripts/clear.js
+++ scripts/clear.js
@@ -2,6 +2,11 @@ const fetch = require("node-fetch");
 
 const url = "https://embedbase-hosted-usx5gpslaq-uc.a.run.app";
 const vaultId = "documentation";
+try {
+    require("dotenv").config();
+} catch (e) {
+    console.log("No .env file found" + e);
+}
 const apiKey = process.env.EMBEDBASE_API_KEY;
 const clear = async () => {
     const response = await fetch(url + "/v1/" + vaultId + "/clear", {
@@ -12,6 +17,7 @@ const clear = async () => {
         },
     });
     const data = await response.json();
+    console.log(data);
 }
 
 clear();
--- scripts/sync.js
+++ scripts/sync.js
@@ -4,6 +4,11 @@ const fetch = require("node-fetch");
 
 const url = "https://embedbase-hosted-usx5gpslaq-uc.a.run.app";
 const vaultId = "documentation";
+try {
+    require("dotenv").config();
+} catch (e) {
+    console.log("No .env file found" + e);
+}
 const apiKey = process.env.EMBEDBASE_API_KEY;
 const sync = async () => {
     // read all files under pages/* with .mdx extension
@@ -13,6 +18,22 @@ const sync = async () => {
         // content of the file
         data: fs.readFileSync(path, "utf-8")
     }));
+    // split documents into chunks of 100 lines
+    // and use id like path/chunkIndex
+    const chunks = [];
+    documents.forEach((document) => {
+        const lines = document.data.split("\n");
+        const chunkSize = 100;
+        for (let i = 0; i < lines.length; i += chunkSize) {
+            const chunk = lines.slice(i, i + chunkSize).join("\n");
+            chunks.push({
+                id: document.id + "/" + i,
+                data: chunk
+            });
+        }
+    });
+
+    console.log("Syncing " + chunks.map((d) => d.id).join(", "));
     const response = await fetch(url + "/v1/" + vaultId, {
         method: "POST",
         headers: {
@@ -24,6 +45,7 @@ const sync = async () => {
         })
     });
     const data = await response.json();
+    console.log(data);
 }
 
 sync();
